fc0fe2d4793df8ed2e235c78a2db50feba2d61fa
Dino Roca <rocalaurented@gmail.com>
2022-11-06 18:22:24 -0500

    Registro de cliente, lado admin

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
 import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreateClienteComponent } from './components/clientes/create-cliente/create-cliente.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
     InicioComponent,
     SidebarComponent,
     LoginComponent,
-    IndexClienteComponent
+    IndexClienteComponent,
+    CreateClienteComponent
   ],
   imports: [
     BrowserModule,

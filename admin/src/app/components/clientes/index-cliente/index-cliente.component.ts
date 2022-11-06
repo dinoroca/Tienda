cb9370c5eddd9242a9e3745b97becc7ae2dbf3c9
Dino Roca <rocalaurented@gmail.com>
2022-11-06 11:55:34 -0500

    protección de rutas para las peticiones post Admin

--- admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from 'src/app/service/cliente.service';
+import { AdminService } from '../../../service/admin.service';
 
 @Component({
   selector: 'app-index-cliente',
@@ -14,17 +15,21 @@ export class IndexClienteComponent implements OnInit {
 
   public page = 1;
   public pageSize = 1;
+  public token;
 
   constructor(
-    private _clienteService: ClienteService
-  ) { }
+    private _clienteService: ClienteService,
+    private _adminService: AdminService
+    ) {
+      this.token = this._adminService.getToken();
+    }
 
   ngOnInit(): void {
     this.init_data();
   }
 
   init_data() {
-    this._clienteService.listar_clientes_filtro_admin(null, null).subscribe(
+    this._clienteService.listar_clientes_filtro_admin(null, null, this.token).subscribe(
       response => {
         this.clientes = response.data;
       },
@@ -39,7 +44,7 @@ export class IndexClienteComponent implements OnInit {
 
     if(tipo == 'apellidos') {
       if(this.filtro_apellidos){
-        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_apellidos).subscribe(
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_apellidos, this.token).subscribe(
           response => {
             this.clientes = response.data;
           },
@@ -53,7 +58,7 @@ export class IndexClienteComponent implements OnInit {
 
     }else if( tipo == 'correo') {
       if(this.filtro_correo){
-        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_correo).subscribe(
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_correo, this.token).subscribe(
           response => {
             this.clientes = response.data;
           },
--- back/controllers/clienteController.js
+++ back/controllers/clienteController.js
@@ -1,90 +1,108 @@
 "use strict";
 
 var Cliente = require("../models/cliente");
-var bcrypt = require('bcrypt-nodejs');
-var jwt = require('../helpers/jwt');
+var bcrypt = require("bcrypt-nodejs");
+var jwt = require("../helpers/jwt");
 
 const registro_cliente = async function (req, res) {
-    //Obtiene los parámetros del cliente
-    var data = req.body;
-    var clientes_arr = [];
+  //Obtiene los parámetros del cliente
+  var data = req.body;
+  var clientes_arr = [];
 
-    //Verifica que no exista correo repetido
-    clientes_arr = await Cliente.find({ email: data.email });
+  //Verifica que no exista correo repetido
+  clientes_arr = await Cliente.find({ email: data.email });
 
-    if (clientes_arr.length == 0) {
-        //Registro del usuario
+  if (clientes_arr.length == 0) {
+    //Registro del usuario
 
-        if(data.password){
-            bcrypt.hash(data.password, null, null, async function(err, hash){
-                if(hash) {
-                    data.password = hash;
-                    var reg = await Cliente.create(data);
-                    res.status(200).send({data: reg});
-                }else{
-                    res.status(200).send({ message: 'Error server', data: undefined });
-                }
-            });
+    if (data.password) {
+      bcrypt.hash(data.password, null, null, async function (err, hash) {
+        if (hash) {
+          data.password = hash;
+          var reg = await Cliente.create(data);
+          res.status(200).send({ data: reg });
         } else {
-            res.status(200).send({ message: 'No hay una contraseña', data: undefined });
+          res.status(200).send({ message: "Error server", data: undefined });
         }
-        
+      });
     } else {
-        res.status(200).send({ message: 'El correo ya existe en la Base de Datos', data: undefined });
+      res
+        .status(200)
+        .send({ message: "No hay una contraseña", data: undefined });
     }
+  } else {
+    res
+      .status(200)
+      .send({
+        message: "El correo ya existe en la Base de Datos",
+        data: undefined,
+      });
+  }
 };
 
-const login_cliente = async function(req, res) {
-    var data = req.body;
-    var clientes_arr = [];
+const login_cliente = async function (req, res) {
+  var data = req.body;
+  var clientes_arr = [];
 
-    //Busca un cliente mediante el correo
-    clientes_arr = await Cliente.find({email: data.email});
+  //Busca un cliente mediante el correo
+  clientes_arr = await Cliente.find({ email: data.email });
 
-    if (clientes_arr.length == 0) {
-        res.status(200).send({ message: 'No se encontró el correo', data: undefined });
-    }else {
-        //Si existe el cliente se manda al login
-        let user = clientes_arr[0];
+  if (clientes_arr.length == 0) {
+    res
+      .status(200)
+      .send({ message: "No se encontró el correo", data: undefined });
+  } else {
+    //Si existe el cliente se manda al login
+    let user = clientes_arr[0];
 
-        //Comparar contraseñas
-        bcrypt.compare(data.password, user.password, async function (error, check){
-            if (check) {
-                res.status(200).send({
-                    data: user,
-                    token: jwt.createToken(user)
-                });
-            }else {
-                res.status(200).send({ message: 'Contraseña incorrecta', data: undefined });
-            }
+    //Comparar contraseñas
+    bcrypt.compare(data.password, user.password, async function (error, check) {
+      if (check) {
+        res.status(200).send({
+          data: user,
+          token: jwt.createToken(user),
         });
-    }
-}
+      } else {
+        res
+          .status(200)
+          .send({ message: "Contraseña incorrecta", data: undefined });
+      }
+    });
+  }
+};
 
 const listar_clientes_filtro_admin = async function (req, res) {
+    
+  if (req.user) {
+    if (req.user.role == "admin") {
 
-    let tipo = req.params['tipo'];
-    let filtro = req.params['filtro'];
+      let tipo = req.params["tipo"];
+      let filtro = req.params["filtro"];
 
-    console.log(tipo);
+      console.log(tipo);
 
-    if (tipo == null || tipo == 'null') {
+      if (tipo == null || tipo == "null") {
         let reg = await Cliente.find();
-        res.status(200).send({data: reg});
-    } else {
-        if (tipo == 'apellidos') {
-            let reg = await Cliente.find({apellidos: new RegExp(filtro, 'i')});
-            res.status(200).send({data: reg});
-        }else if (tipo == 'correo'){
-            let reg = await Cliente.find({email: new RegExp(filtro, 'i')});
-            res.status(200).send({data: reg});
+        res.status(200).send({ data: reg });
+      } else {
+        if (tipo == "apellidos") {
+          let reg = await Cliente.find({ apellidos: new RegExp(filtro, "i") });
+          res.status(200).send({ data: reg });
+        } else if (tipo == "correo") {
+          let reg = await Cliente.find({ email: new RegExp(filtro, "i") });
+          res.status(200).send({ data: reg });
         }
+      }
+    } else {
+        res.status(500).send({message: 'NoAccess'});
     }
-
-}
+  }else {
+    res.status(500).send({message: 'NoAccess'});
+  }
+};
 
 module.exports = {
-    registro_cliente,
-    login_cliente,
-    listar_clientes_filtro_admin
+  registro_cliente,
+  login_cliente,
+  listar_clientes_filtro_admin,
 };

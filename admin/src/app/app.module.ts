9b7801c39f079600c7a2d318ae1582e8609a4bab
Dino Roca <rocalaurented@gmail.com>
2022-11-06 09:51:06 -0500

    Listado, filtro y paginación de clientes

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -9,19 +9,23 @@ import { routing } from './app.routing';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
+import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     SidebarComponent,
-    LoginComponent
+    LoginComponent,
+    IndexClienteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    NgbPaginationModule,
     routing
   ],
   providers: [],
---
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/service/cliente.service';
+
+@Component({
+  selector: 'app-index-cliente',
+  templateUrl: './index-cliente.component.html',
+  styleUrls: ['./index-cliente.component.css']
+})
+export class IndexClienteComponent implements OnInit {
+
+  public clientes : Array<any> = [];
+  public filtro_apellidos = '';
+  public filtro_correo = '';
+
+  public page = 1;
+  public pageSize = 1;
+
+  constructor(
+    private _clienteService: ClienteService
+  ) { }
+
+  ngOnInit(): void {
+    this.init_data();
+  }
+
+  init_data() {
+    this._clienteService.listar_clientes_filtro_admin(null, null).subscribe(
+      response => {
+        this.clientes = response.data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  filtro(tipo: any) {
+
+
+    if(tipo == 'apellidos') {
+      if(this.filtro_apellidos){
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_apellidos).subscribe(
+          response => {
+            this.clientes = response.data;
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }else {
+        this.init_data();
+      }
+
+    }else if( tipo == 'correo') {
+      if(this.filtro_correo){
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_correo).subscribe(
+          response => {
+            this.clientes = response.data;
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.init_data();
+      }
+    }
+
+  }
+
+}

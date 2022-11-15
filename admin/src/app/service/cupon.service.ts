64b6ef79edd664013315a9672b1c4c9adc7d112a
Dino Roca <rocalaurented@gmail.com>
2022-11-15 12:02:01 -0500

    Actualizar cupón lada admin

--- admin/src/app/service/cupon.service.ts
+++ admin/src/app/service/cupon.service.ts
@@ -26,4 +26,14 @@ export class CuponService {
     let headers = new HttpHeaders({'Content-Type':'application/json', 'authorization': token});
     return this._http.get(this.url + 'listar_cupones_admin/' + filtro, {headers : headers});
   }
+
+  obtener_cupon_admin(id: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({'Content-Type':'application/json', 'authorization': token});
+    return this._http.get(this.url + 'obtener_cupon_admin/' + id, {headers : headers});
+  }
+
+  actualizar_cupon_admin(id: any, data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({'Content-Type':'application/json', 'authorization': token});
+    return this._http.put(this.url + 'actualizar_cupon_admin/' + id, data, {headers : headers});
+  }
 }

baef2e6ac07636cea0964bf88ab5e8dfcaff9ada
Dino Roca <rocalaurented@gmail.com>
2022-11-27 23:06:48 -0500

    Registro de dirección lado cliente

--- tienda/src/app/components/usuario/direcciones/direcciones.component.ts
+++ tienda/src/app/components/usuario/direcciones/direcciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { GuestService } from '../../../services/guest.service';
 
 declare var iziToast: { show: (arg0: { title: string; titleColor: string; class: string; position: string; message: string; }) => void; };
@@ -20,17 +21,40 @@ export class DireccionesComponent implements OnInit {
     region: '',
     provincia: '',
     distrito: '',
-    principal: true,
+    principal: false,
   };
 
   public regiones: Array<any> = [];
   public provincias: Array<any> = [];
   public distritos: Array<any> = [];
 
+  public regiones_arr: Array<any> = [];
+  public provincias_arr: Array<any> = [];
+  public distritos_arr: Array<any> = [];
+
   constructor(
-    private _guestService: GuestService
+    private _guestService: GuestService,
+    private _clienteService: ClienteService
   ) {
     this.token = localStorage.getItem('token');
+
+    this._guestService.obtener_regiones().subscribe(
+      response => {
+        this.regiones_arr = response;
+      }
+    );
+
+    this._guestService.obtener_provincias().subscribe(
+      response => {
+        this.provincias_arr = response;
+      }
+    );
+
+    this._guestService.obtener_distritos().subscribe(
+      response => {
+        this.distritos_arr = response;
+      }
+    );
   }
 
   ngOnInit(): void {
@@ -94,6 +118,77 @@ export class DireccionesComponent implements OnInit {
         });
       }
     );
+  }
+
+  registrar(registroForm: any) {
+
+    this.regiones_arr.forEach(element => {
+      if (parseInt(element.id) == parseInt(this.direccion.region)) {
+        this.direccion.region = element.name;
+      }
+    });
+
+    this.provincias_arr.forEach(element => {
+      if (parseInt(element.id) == parseInt(this.direccion.provincia)) {
+        this.direccion.provincia = element.name;
+      }
+    });
+
+    this.distritos_arr.forEach(element => {
+      if (parseInt(element.id) == parseInt(this.direccion.distrito)) {
+        this.direccion.distrito = element.name;
+      }
+    });
+
+    if (registroForm.valid) {
+      let data = {
+        destinatario: this.direccion.destinatario,
+        dni: this.direccion.dni,
+        zip: this.direccion.zip,
+        telefono: this.direccion.telefono,
+        direccion: this.direccion.direccion,
+        pais: this.direccion.pais,
+        region: this.direccion.region,
+        provincia: this.direccion.provincia,
+        distrito: this.direccion.distrito,
+        principal: this.direccion.principal,
+        cliente: localStorage.getItem('_id')
+      }
+
+      this._clienteService.registro_direccion_cliente(data, this.token).subscribe(
+        response => {
+          this.direccion = {
+            pais: '',
+            region: '',
+            provincia: '',
+            distrito: '',
+            principal: false,
+          };
+
+          $('#region').prop('disabled', true);
+          $('#provincia').prop('disabled', true);
+          $('#distrito').prop('disabled', true);
+
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#35D18F',
+            class: 'text-success',
+            position: 'topRight',
+            message: 'Se registró la nueva dirección'
+          });
+
+        }
+      );
+
+    } else {
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FF634F',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Los datos del formulario no son válidos'
+      });
+    }
   }
 
 }

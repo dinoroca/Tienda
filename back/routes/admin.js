b2ca47b43be2b2533ae48ed788f7eee119f696e1
Dino Roca <rocalaurented@gmail.com>
2022-12-31 19:10:40 -0500

    Obtener venta de programas lado admin y confirmar pago y eliminar reservados

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -19,6 +19,13 @@ api.put('/actualizar_ventas_enviado_admin/:id', auth.auth, adiminController.actu
 api.put('/actualizar_ventas_procesando_admin/:id', auth.auth, adiminController.actualizar_ventas_procesando_admin);
 api.put('/actualizar_ventas_recibido_admin/:id', auth.auth, adiminController.actualizar_ventas_recibido_admin);
 
+//Software
+api.get('/obtener_ventas_software_admin/:desde?/:hasta?', auth.auth, adiminController.obtener_ventas_software_admin);
+api.get('/obtener_venta_software_admin/:id?', auth.auth, adiminController.obtener_venta_software_admin);
+
+api.put('/actualizar_venta_software_pagado_admin/:id', auth.auth, adiminController.actualizar_venta_software_pagado_admin);
+api.delete('/eliminar_venta_software_admin/:id', auth.auth, adiminController.eliminar_venta_software_admin);
+
 api.get('/kpi_ganancias_mensuales_admin', auth.auth, adiminController.kpi_ganancias_mensuales_admin);
 api.get('/kpi_ganancias_programas_admin', auth.auth, adiminController.kpi_ganancias_programas_admin);
 
